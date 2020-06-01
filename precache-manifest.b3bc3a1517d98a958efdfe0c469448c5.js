744620f69bed2d961110e51865ef33f5a9f2cf33
RashidAlackal <asrashid4@gmail.com>
2020-06-01 21:49:59 +0530

    Updates

---
+++ precache-manifest.b3bc3a1517d98a958efdfe0c469448c5.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995d4559bf96b792f56cee6e39dfab42",
+    "url": "/agency-portfolio/index.html"
+  },
+  {
+    "revision": "ec9483571e19a81bf070",
+    "url": "/agency-portfolio/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c918fd38cb1d606678f0",
+    "url": "/agency-portfolio/static/css/main.1a8b60e5.chunk.css"
+  },
+  {
+    "revision": "ec9483571e19a81bf070",
+    "url": "/agency-portfolio/static/js/2.69e40812.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/agency-portfolio/static/js/2.69e40812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c918fd38cb1d606678f0",
+    "url": "/agency-portfolio/static/js/main.42dc5993.chunk.js"
+  },
+  {
+    "revision": "647014d58fe5989a5df2",
+    "url": "/agency-portfolio/static/js/runtime-main.b350015f.js"
+  },
+  {
+    "revision": "e78073c3198e949f4462ff9c1652048c",
+    "url": "/agency-portfolio/static/media/about1.e78073c3.jpg"
+  },
+  {
+    "revision": "b8e394002b38f3c4f68a09f91e31c102",
+    "url": "/agency-portfolio/static/media/about2.b8e39400.jpg"
+  },
+  {
+    "revision": "c0cd138271276bb33a87c2d174015455",
+    "url": "/agency-portfolio/static/media/facebook.c0cd1382.svg"
+  },
+  {
+    "revision": "0e34a6b20d28325e6ade6297bb98de38",
+    "url": "/agency-portfolio/static/media/img4.0e34a6b2.jpg"
+  },
+  {
+    "revision": "fc23b90236f87b11147810f35f4c5293",
+    "url": "/agency-portfolio/static/media/instagram.fc23b902.svg"
+  },
+  {
+    "revision": "aeffd6776fe2574b2c093abd35d444c2",
+    "url": "/agency-portfolio/static/media/monitor.aeffd677.png"
+  },
+  {
+    "revision": "50d589f32ec07ec444bbe76a846ef95b",
+    "url": "/agency-portfolio/static/media/play-button.50d589f3.svg"
+  },
+  {
+    "revision": "a7a63ab038ec8685238bfb5926944e58",
+    "url": "/agency-portfolio/static/media/tablet.a7a63ab0.png"
+  },
+  {
+    "revision": "c3125cb9a4b9815e5dd6dd657c5c0bbc",
+    "url": "/agency-portfolio/static/media/team2.c3125cb9.jpg"
+  },
+  {
+    "revision": "3820ac90ff33c4fda05cd77df8a4b032",
+    "url": "/agency-portfolio/static/media/team3.3820ac90.jpg"
+  },
+  {
+    "revision": "d6f4c88e34fbc1e1d009dcf4079e5f8f",
+    "url": "/agency-portfolio/static/media/team4.d6f4c88e.jpg"
+  },
+  {
+    "revision": "76c75acc2e2b1d5e2cbbc2736af3cb1c",
+    "url": "/agency-portfolio/static/media/twitter.76c75acc.svg"
+  },
+  {
+    "revision": "adf3232ab48699d4cfcaf923df8b1e36",
+    "url": "/agency-portfolio/static/media/work0.adf3232a.jpg"
+  },
+  {
+    "revision": "40c3553aaeeeb96186d4c045bcfb2e29",
+    "url": "/agency-portfolio/static/media/work1.40c3553a.jpg"
+  },
+  {
+    "revision": "5c1f0b32dfc6be4c404198c05fa13d86",
+    "url": "/agency-portfolio/static/media/work10.5c1f0b32.jpg"
+  },
+  {
+    "revision": "8bb630d4aad40ad2609251eb682a78ab",
+    "url": "/agency-portfolio/static/media/work11.8bb630d4.jpg"
+  },
+  {
+    "revision": "6c9617eaf4001f905c482ae1724dc43d",
+    "url": "/agency-portfolio/static/media/work12.6c9617ea.jpg"
+  },
+  {
+    "revision": "c3e1553faea03c2deb5db171f0a95bba",
+    "url": "/agency-portfolio/static/media/work2.c3e1553f.jpg"
+  },
+  {
+    "revision": "bcf312c3bfda33bd3e72d0f0c5513ff3",
+    "url": "/agency-portfolio/static/media/work3.bcf312c3.jpg"
+  },
+  {
+    "revision": "909d6f41f9636d2ac2d43ed57b1bcdaa",
+    "url": "/agency-portfolio/static/media/work4.909d6f41.jpg"
+  },
+  {
+    "revision": "65a7b3515762cef54fdaecbd818b3711",
+    "url": "/agency-portfolio/static/media/work5.65a7b351.jpg"
+  },
+  {
+    "revision": "4fdfe8e990005f58350621e4d029d4a2",
+    "url": "/agency-portfolio/static/media/work6.4fdfe8e9.jpg"
+  },
+  {
+    "revision": "2f4c9ceb3edc12836f2dc18b1dfbac46",
+    "url": "/agency-portfolio/static/media/work7.2f4c9ceb.jpg"
+  },
+  {
+    "revision": "e33503db6f93e1dd5d6856c9d2da2c39",
+    "url": "/agency-portfolio/static/media/work8.e33503db.jpg"
+  },
+  {
+    "revision": "c41b86b9101ad37ff10999339392d4c7",
+    "url": "/agency-portfolio/static/media/work9.c41b86b9.jpg"
+  }
+]);
